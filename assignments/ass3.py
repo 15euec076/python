3fe1b00e3435c61496e6efe5395521b0f3c6e5ee
15euec076 <32997235+15euec076@users.noreply.github.com>
2017-12-26 16:17:54 +0530

    Create ass3.py

---
+++ assignments/ass3.py
@@ -0,0 +1,6 @@
+bill_id=1001
+customer_id=101
+bill_amount=199.99
+print(" type of bill_id:",type(bill_id))
+print("type of customer_id:",type(customer_id))
+print("type of bill_amount:Rs",type(bill_amount))
